11fe071aaf3a7c0f665c4d638ce00d9b260276d2
Kirill Sayed <sayed.kirill@gmail.com>
2022-11-28 20:00:02 +0300

    Added @Service annotation to AppUserServiceImpl

--- appuser/src/main/java/com/newsocial/appuser/AppUserServiceImpl.java
+++ appuser/src/main/java/com/newsocial/appuser/AppUserServiceImpl.java
@@ -4,8 +4,10 @@ import com.newsocial.appuser.exceptions.AppUserNotFoundException;
 import com.newsocial.appuser.exceptions.RoleNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class AppUserServiceImpl implements AppUserService {
 
